703018344f2cdddd5b4a7cfa8c8bf53fc211be39
MAnd <48105724+chrmand@users.noreply.github.com>
2022-03-09 19:56:15 +0200

    add files for HotelReservationApp

---
+++ HotelReservationApp.java
@@ -0,0 +1,16 @@
+package hotelreservationapp;
+
+/**
+ *
+ * @author Χρήστος
+ */
+public class HotelReservationApp {
+
+    
+    public static void main(String[] args) {
+       
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.setVisible(true);
+    }
+    
+}
